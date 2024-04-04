8651a986b1157892eaebb42c35924120433ca92d
mahasowmi12 <131862860+mahasowmi12@users.noreply.github.com>
2024-04-04 19:34:31 +0530

    Delete Dribble/home directory

--- Dribble/home/script 2.js
+++
@@ -1,8 +0,0 @@
-// Get the finish button
-const finishButton = document.getElementById('finishButton');
-
-// Add an event listener to the finish button
-finishButton.addEventListener('click', function() {
-  // Display an alert when the button is clicked
-  alert('Thank you for your selection!');
-});
